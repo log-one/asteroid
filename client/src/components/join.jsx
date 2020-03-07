f5928704f7a25dc36707f2b00e742609e1d00664
V <twovsmakeaw@protonmail.com>
2020-03-07 16:46:31 -0500

    expanded Join.jsx and added state to it

--- client/src/components/join.jsx
+++ client/src/components/join.jsx
@@ -1,7 +1,40 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Join = () => {
-  return <h1>join</h1>;
+  //add state variables
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  //describe the view
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder=""
+            className="joinInput"
+            type="text"
+            onChange={event => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder=""
+            className="joinInput mt-20"
+            type="text"
+            onChange={event => setRoom(event.target.value)}
+          />
+        </div>
+        <Link to={`/chat?name=${name}&room=${room}`}>
+          <button className="button mt-20" type="submit">
+            Sign in
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
