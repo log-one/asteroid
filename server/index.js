dd02ebd294487823114e4015527e02151223de13
V <twovsmakeaw@protonmail.com>
2020-03-07 15:00:10 -0500

    added code to manage a connection and disconnection to a socket

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server); //now we have a socket.io object that can do a lot of stuff
 
+//The disconnect function inside of the io.on() because we are managing that particular socket that was connected.
+io.on("connection", socket => {
+  console.log("We have a new connection!!!");
+
+  socket.on("disconnect", () => {
+    console.log("User just left!!!");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
