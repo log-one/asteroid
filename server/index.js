699a19d3818bfa6d1a61ac8d8807e114af034759
V <twovsmakeaw@protonmail.com>
2020-03-08 16:41:54 -0400

    added 'join' and 'sendMessage' event handler and 'message' event emitter

--- server/index.js
+++ server/index.js
@@ -16,16 +16,42 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.jsx");
 
 //The disconnect function inside of the io.on() because we are managing that particular socket that was connected.
 io.on("connection", socket => {
-  console.log("We have a new connection!!!");
-
   socket.on("join", (joinData, callback) => {
-    console.log(joinData);
+    const { error, user } = addUser({
+      id: socket.id,
+      name: joinData.name,
+      room: joinData.room
+    }); //add user returns either a error object or user object
 
     //a callback function can be passed in along with the data
     //this callback can be executed after something has been done with the data
-    //for ex: error handling
-    const error = true;
-    if (error) callback({ error: "error" });
+    if (error) return callback(error);
+
+    //here we emit an event from the backend to the frontend
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+
+    //socket.broadcast is going to send a message to everyone besides that specific user
+    //again we emit an event from the backend to the frontend
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    //socket.join() is a built in function that joins a user into a room
+    socket.join(user.room);
+
+    callback();
+  });
+
+  //handling an event recieved by the backend from the frontend
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback(); //to do something after the message is sent
   });
 
   socket.on("disconnect", () => {
