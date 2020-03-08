d0409251abd7ead7b5058e371866dd3ba33de050
V <twovsmakeaw@protonmail.com>
2020-03-08 16:13:10 -0400

    created users.jsx on server side which contains helper funcs to manage users

---
+++ server/users.jsx
@@ -0,0 +1,42 @@
+//use this file to create helper functions to manage users
+
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  //check if a user with the same name already exists in the room
+  const existingUser = users.find(
+    user => user.room === room && user.name === name
+  );
+
+  //if true, exit the function and return an error
+  if (existingUser) {
+    return { error: "Username is taken" };
+  }
+
+  // else make a 'user' object and push it into the 'users' array
+  const user = { id, name, room };
+  users.push(user);
+
+  return { user };
+};
+
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const getUser = id => {
+  users.find(user => user.id === id);
+};
+
+const getUserInRoom = room => {
+  users.filter(user => user.room === room);
+};
+
+module.exports = { addUser, removeUser, getUser, getUserInRoom };
