c2a89897614d2f70ec8760dabb07940200c2f1e7
V <twovsmakeaw@protonmail.com>
2020-03-08 16:12:18 -0400

    corrected Join.jsx | expanded Chat.jsx, created socket and socket emitter -> added socket event listener on server index.js

--- client/src/components/Chat/chat.jsx
+++ client/src/components/Chat/chat.jsx
@@ -1,8 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
 import "./chat.css";
 
-const Chat = () => {
+let socket;
+//the location prop comes from React-Router
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    //location.search contains just the query part of the URL
+    //queryString.parse() parses it into an object with the query string parameters and values as key-value pairs
+    const { name, room } = queryString.parse(location.search);
+
+    //create a socket object on the client side
+    socket = io(ENDPOINT);
+    //this socket object is connected to the socket object on the server side through the endpoint URL
+
+    setName(name);
+    setRoom(room);
+
+    //socket.emit() allows us to emit an event to the server and have some data passed along with it
+    //the server upon recognizing the emitted event can access the emitted data (if any) and do something
+    //a callback function can be passed in the server as the second parameter of the event listener
+    //we can define that callback function here to do something after the event has been emitted (ex: to do error handling)
+    socket.emit("join", { name, room }, error => {
+      alert(error.error);
+    });
+
+    //describe what needs to be done as the component unmounts
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
+
   return <h1>chat</h1>;
 };
 
--- server/index.js
+++ server/index.js
@@ -12,9 +12,21 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server); //now we have a socket.io object that can do a lot of stuff
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.jsx");
+
 //The disconnect function inside of the io.on() because we are managing that particular socket that was connected.
 io.on("connection", socket => {
   console.log("We have a new connection!!!");
+
+  socket.on("join", (joinData, callback) => {
+    console.log(joinData);
+
+    //a callback function can be passed in along with the data
+    //this callback can be executed after something has been done with the data
+    //for ex: error handling
+    const error = true;
+    if (error) callback({ error: "error" });
+  });
 
   socket.on("disconnect", () => {
     console.log("User just left!!!");
