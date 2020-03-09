4c15452b9684707c213b4a3541a97bbdd58e15cc
V <twovsmakeaw@protonmail.com>
2020-03-09 17:52:21 -0400

    added the InputBar component to Chat.jsx

--- client/src/components/Chat/chat.jsx
+++ client/src/components/Chat/chat.jsx
@@ -2,6 +2,7 @@ import React, { Component, useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 import InfoBar from "../InfoBar/InfoBar";
+import InputBar from "../InputBar/InputBar";
 
 import "./chat.css";
 
@@ -71,10 +72,10 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-        <input
-          value={message} //this makes the input field empty again when the 'message' state becomes an empty string every time a message is sent
-          onChange={e => setMessage(e.target.value)}
-          onKeyPress={e => (e.key === "Enter" ? sendMessage(e) : null)}
+        <InputBar
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
         />
       </div>
     </div>
---
+++ client/src/components/InputBar/InputBar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./InputBar.css";
+
+const InputBar = ({ message, setMessage, sendMessage }) => {
+  return (
+    <form className="form">
+      \
+      <input
+        className="input"
+        type="text"
+        placeholder="Type a message..."
+        value={message} //this makes the input field empty again when the 'message' state becomes an empty string every time a message is sent
+        onChange={e => setMessage(e.target.value)}
+        onKeyPress={e => (e.key === "Enter" ? sendMessage(e) : null)}
+      />
+      <button className="sendButton" onClick={event => sendMessage(event)}>
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default InputBar;
