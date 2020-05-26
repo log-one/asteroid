a471582945c0619d39ab197daecb319d9bfe07c9
V <twovsmakeaw@protonmail.com>
2020-05-26 01:09:40 -0400

    added new useEffects and updated state variables

--- client/src/components/Chat/chat.jsx
+++ client/src/components/Chat/chat.jsx
@@ -16,12 +16,10 @@ const Chat = ({ location }) => {
   const [messages, setMessages] = useState([]); //array of all messages
   //above array of messages should be in a DB
   const [message, setMessage] = useState(""); // each message
-  const [users, setUsers] = useState([]); //array of users in room
+  // const [enqueued, setEnqueued] = useState(false);
   const [sideBarOpen, setSideBarOpen] = useState(false);
-  const [canSpeak, setCanSpeak] = useState({
-    eligible: true,
-    lastMessageNum: 0,
-  });
+  const [canSpeak, setCanSpeak] = useState(true);
+  const [peers, setPeers] = useState([]); //update
 
   const ENDPOINT = "localhost:5000";
 
@@ -31,20 +29,19 @@ const Chat = ({ location }) => {
     console.log("JOIN (first) useEffect()");
     //location.search contains just the query part of the URL
     //queryString.parse() parses it into an object with the query string parameters and values as key-value pairs
-    const { name, room } = queryString.parse(location.search);
+    const { name } = queryString.parse(location.search);
 
     //create a socket object on the client side
     socket = io(ENDPOINT); //emits a 'connection' event to ENDPOINT along with 'socket' object?
     //this socket object is connected to the socket object on the server side through the endpoint URL
 
     setName(name.trim().toLowerCase());
-    setRoom(room);
 
     //socket.emit() allows us to emit an event to the server and have some data passed along with it
     //the server upon recognizing the emitted event can access the emitted data (if any) and do something
     //a callback function can be passed in the server as the second parameter of the event listener
     //we can define that callback function here to do something after the event has been emitted (ex: to do error handling)
-    socket.emit("join", { name, room }, (e) => {
+    socket.emit("join", { name }, (e) => {
       alert(e);
     });
 
@@ -62,57 +59,71 @@ const Chat = ({ location }) => {
   //By the end of the execution of the first useEffect, a user would've been added to the Users[] in the backend and
   //a 'message' event would've been sent by {user: 'admin'}, welcoming the new user.
 
-  //this useEffect() updates data about users in room and re-renders sidebar
+  // this useEffect() updates data about peers in room and re-renders the green dot
   useEffect(() => {
-    console.log("ROOMDATA (SECOND) useEffect()");
-    socket.on("roomData", ({ users }) => {
-      console.log("UPDATINGGGG USERS (2nd EFFECT", users);
-      setUsers(users); //updates state of users with data recieved from getUsersInRoom() from server
-      console.log("UPDATED USERS (2nd EFFECT", users);
+    socket.on("peerChange", (newPeers) => {
+      setPeers((peers) => {
+        peers.splice(0, peers.length, ...newPeers);
+        return peers;
+      });
     });
 
     return () => {
-      socket.off("roomData");
+      socket.off("peerChange");
     };
-  }, [users]);
+  }, [peers]);
+
+  //this UseEffect() handles changes in the user's room and loads stored messages from database
+
+  useEffect(() => {
+    socket.on("room-change", (room, oldMessages) => {
+      setRoom(room);
+      setMessages((messages) => {
+        messages.splice(0, messages.length, ...oldMessages);
+        return messages;
+      });
+    });
+
+    return () => {
+      socket.off("room-change");
+    };
+  }, [room, messages]);
 
   //the next useEffect() handles the 'message' event and updates the Messages[] state of the Chat component
   useEffect(() => {
     console.log("MESSAGE (THIRD) useEffect()");
     socket.on("message", (message) => {
-      console.log("UPDATINGGGGG MSGS");
+      socket.emit("update-messages", message);
       setMessages([...messages, message]); //this is adding every new message sent by admin or anyone else to our messages array
-      console.log("UPDATED MSGS (3rd EFFECT");
       //convert later to promises or async await
     });
 
-    //disable input if most recent message was sent by current user
-    if (messages.length !== 0)
-      if (messages[messages.length - 1].user.name === name) {
-        setCanSpeak({
-          eligible: false,
-          lastMessageNum: messages.length,
-        }); //when user speaks, note what number his message is in the list of all messages
-      }
-    //turn-based conversation logic
-    let messagesSince;
-    //after new message from another user is rendered calculate how many messages have passed since the current user last spoke
-    messagesSince = messages.length - canSpeak.lastMessageNum;
-    //decide when to allow users to speak
-    if (users.length === 2 && messagesSince === 1)
-      setCanSpeak({
-        eligible: true,
-        lastMessageNum: canSpeak.lastMessageNum,
+    //event listener to clear messages
+    socket.on("clear-messages", () => {
+      setMessages((messages) => {
+        messages.length = 0;
+        return messages;
       });
-    if (users.length > 2 && messagesSince === 2)
-      setCanSpeak({
-        eligible: true,
-        lastMessageNum: canSpeak.lastMessageNum,
+    });
+
+    //event listener to load previous messages
+    socket.on("load-prev-messages", (oldMessages) => {
+      setMessages((messages) => {
+        messages.splice(0, messages.length, ...oldMessages);
+        return messages;
       });
+    });
+
+    //make user eligible to speak if most recent message was sent by peer
+    if (messages.length > 0)
+      if (messages[messages.length - 1].user === name) setCanSpeak(false);
+      else setCanSpeak(true);
 
     return () => {
       socket.off("message");
+      socket.off("clear-messages");
     };
+    // eslint-disable-next-line
   }, [messages]);
 
   //function to toggle sidebar
@@ -126,7 +137,21 @@ const Chat = ({ location }) => {
     event.preventDefault(); //clicking a button or onKeyPress refreshes the whole page. This prevents that default behaviour from happening
 
     if (message) {
-      socket.emit("sendMessage", message, () => setMessage("")); //the callback function resets the message state to an empty string
+      if (message === "#chat") {
+        socket.emit("#chat", () => setMessage(""));
+      } else if (message === "#home") {
+        socket.emit("#home", () => setMessage(""));
+      } else if (message === "#skip") {
+        socket.emit("#skip", () => setMessage(""));
+      } else if (message === "#room/new") {
+        socket.emit("#room/new", () => setMessage(""));
+      } else if (message.slice(0, 11) === "#room/join/") {
+        const roomKey = message.slice(11, message.length);
+        socket.emit("#room/join", roomKey, () => setMessage(""));
+      } else if (message.slice(0, 13) === "#room/delete/") {
+        const roomKey = message.slice(13, message.length);
+        socket.emit("#room/delete", roomKey, () => setMessage(""));
+      } else socket.emit("sendMessage", message, () => setMessage("")); //the callback function resets the message state to an empty string
     }
   };
 
@@ -138,24 +163,22 @@ const Chat = ({ location }) => {
         <SideBar
           sideBarOpen={sideBarOpen}
           toggleSideBar={toggleSideBar}
-          users={users}
-          room={room}
+          peers={peers}
         />
 
         <InfoBar
-          room={room}
           name={name}
-          users={users}
           toggleSideBar={toggleSideBar}
           setCanSpeak={setCanSpeak}
           canSpeak={canSpeak}
           sideBarOpen={sideBarOpen}
           messages={messages}
+          peers={peers}
         />
 
         <Messages messages={messages} name={name} />
         <InputBar
-          pattern={"(^[ a-z0-9]{2,100}$)|(^<3$)|(^#news$)"}
+          pattern={"(^[ a-z0-9]{2,100}$)|(^<3$)|(^#news$)|(^#chat$)"}
           message={message}
           name={name}
           setMessage={setMessage}
